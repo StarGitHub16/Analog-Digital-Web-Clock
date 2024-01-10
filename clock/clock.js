63d1667abff8aba7ccb8707fc9354efa41f077d9
StarGitHub16 <114962374+StarGitHub16@users.noreply.github.com>
2024-01-09 19:05:57 -0600

    Fully functional clock By Star

---
+++ clock/clock.js
@@ -0,0 +1,33 @@
+setInterval(setClock, 1000)
+setInterval(setDigitalClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+function setDigitalClock() {
+
+    let currentTime = new Date();
+    hrs.innerHTML = currentTime.getHours(); 
+    mins.innerHTML = currentTime.getMinutes();
+    secs.innerHTML = currentTime.getSeconds();   
+}
+
+setClock()
+
+setDigitalClock()
